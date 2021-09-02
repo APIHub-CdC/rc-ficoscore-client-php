4edfef54dd1d1b9acee609a862e38cb11a3aaafa
gamaliellobato <gamaliel.lobato.eon@gmail.com>
2021-09-02 15:01:13 -0500

    Se agrega cliente rc ficoscore php

---
+++ lib/Model/CatalogoSexo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace rc\ficoscore\Client\Model;
+use \rc\ficoscore\Client\ObjectSerializer;
+
+class CatalogoSexo
+{
+    
+    const F = 'F';
+    const M = 'M';
+    
+    
+    public static function getAllowableEnumValues()
+    {
+        return [
+            self::F,
+            self::M,
+        ];
+    }
+}
